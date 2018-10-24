9e9b663960a95ed33928661f444d4aaa3824a5d7
billikill <33964260+billikill@users.noreply.github.com>
2018-10-24 13:20:49 +0300

    Update inRole.groovy
    Change Validator

--- inRole.groovy
+++ inRole.groovy
@@ -17,13 +17,13 @@ import com.atlassian.query.clause.TerminalClause
 import com.atlassian.query.operand.FunctionOperand
 import com.atlassian.jira.issue.link.IssueLink
 import groovy.json.JsonSlurper
-import is.origo.jira.plugin.common.TempoWorklogManager
 import com.onresolve.scriptrunner.runner.customisers.PluginModule
 import com.onresolve.scriptrunner.runner.customisers.WithPlugin
-import com.tempoplugin.team.api.event.TeamRoleEvent
+
+import com.tempoplugin.team.api.TeamManager
 @WithPlugin("is.origo.jira.tempo-plugin")
 @PluginModule
-TempoWorklogManager tempoWorklogManager
+TeamManager teamManager
 
 class inRole extends AbstractScriptedJqlFunction implements JqlFunction {
 
@@ -40,10 +40,10 @@ class inRole extends AbstractScriptedJqlFunction implements JqlFunction {
                         description: "Name Team",
                         optional: false,
                 ],
-                [
-                        description: "Name Role",
-                        optional: false,
-                ],
+                //[
+                //        description: "Name Role",
+                //        optional: false,
+                //],
         ]
     }
 
@@ -61,10 +61,10 @@ class inRole extends AbstractScriptedJqlFunction implements JqlFunction {
 
     @Override
     MessageSet validate(ApplicationUser user, FunctionOperand operand, TerminalClause terminalClause) {
-        Issue issue = ComponentAccessor.getIssueManager().getIssueObject(operand.args.first())
+        def name_team = teamManager.getTeamByName(operand.args.first())
         MessageSet messages = new MessageSetImpl()
         if (issue == null) {
-            messages.addErrorMessage ("Issue not found")
+            messages.addErrorMessage ("Team not found")
         }
         return messages
     }
@@ -74,28 +74,7 @@ class inRole extends AbstractScriptedJqlFunction implements JqlFunction {
     List<QueryLiteral> getValues( QueryCreationContext queryCreationContext, FunctionOperand operand, TerminalClause terminalClause) {
 
         List<QueryLiteral> out = []
-        def name_team = null
-        def name_role = null
-        def conf = null
-        authenticatedRequestFactory
-                .createRequest(Request.MethodType.GET, "https://jira.xxx.ru/rest/tempo-teams/2/team/" + name_team + "/member" )
-                .addHeader("Content-Type", "application/json")
-                .execute(new ResponseHandler<Response>() {
-            @Override
-            void handle(Response response) throws ResponseException {
-                conf = new JsonSlurper().parseText(response.getResponseBodyAsString())
-            }
-        })
-        log.debug("Список участников команды с ролями :" + conf.member)
-
-        def params =[
-                member: name,
-                membership: [
-                        role: [
-                                name: name_role,
-                        ]
-                ]
-        ]
+        
 
 
 
