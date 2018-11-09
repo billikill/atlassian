b3a8ef406162df67d0d59af18d9335accd0e985d
alex <bazhenov_alex@list.ru>
2018-11-09 16:56:03 +0300

    Change Script

--- TeamInRole.groovy
+++ TeamInRole.groovy
@@ -16,18 +16,18 @@ import com.onresolve.jira.groovy.jql.AbstractScriptedJqlFunction
 import com.onresolve.scriptrunner.runner.ScriptRunnerImpl
 import com.onresolve.scriptrunner.runner.customisers.PluginModule
 import com.onresolve.scriptrunner.runner.customisers.WithPlugin
+import com.tempoplugin.core.datetime.range.LocalDateRange
+import com.tempoplugin.platform.api.user.TempoUser
 import com.tempoplugin.team.api.TeamManager
 import com.tempoplugin.team.api.TeamService
-import com.tempoplugin.team.api.role.TeamRole
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 
-
-
-@WithPlugin ("com.tempoplugin.tempo-teams")
+@WithPlugin ("is.origo.jira.tempo-plugin")
 class TeamInRole extends AbstractScriptedJqlFunction implements JqlFunction {
-    TeamService teamService  = ScriptRunnerImpl.getPluginComponent(TeamService)
     TeamManager teamManager  = ScriptRunnerImpl.getPluginComponent(TeamManager)
-
+    TeamService teamService  = ScriptRunnerImpl.getPluginComponent(TeamService)
 
     @Override
     String getDescription() {
@@ -60,6 +60,7 @@ class TeamInRole extends AbstractScriptedJqlFunction implements JqlFunction {
 
         MessageSet messages = new MessageSetImpl()
         def team = teamManager.getTeamByName(operand.args.get(0))
+
         def name_role = operand.args.get(1)
         def roles = teamService.getTeamRoles().get()
         boolean roleExists = false
@@ -80,18 +81,54 @@ class TeamInRole extends AbstractScriptedJqlFunction implements JqlFunction {
     @Override
     List<QueryLiteral> getValues( QueryCreationContext queryCreationContext, FunctionOperand operand, TerminalClause terminalClause) {
 
-        def team = teamService.getTeamByName(operand.args.get(0))       // получили название команды, которую ввел юзер
+        def log = Logger.getLogger("com.tempoplugin.CreateJQL")
+        log.setLevel(Level.DEBUG)
+
+        def team = teamManager.getTeamByName(operand.args.get(0))       // получили название команды, которую ввел юзер
         def team_id =  team.getId()                                     // получаем id команды
+        log.debug ("ID команды котоую ввел юзер:" + team_id)
         def name_role = operand.args.get(1)                             // получили название роли, которую ввел юзер
-        //def roles = teamService.getTeamRoles().get()                    // получили все роли какие есть
-        //def members= teamManager.getActiveTempoMembers(team)            // получили учатников команды
-        //def members_role = teamService.getTeamMembersByRole(name_role)  // получили участников команды в роли, которую ввел юзер
-        def team_member = teamService.getTeamMembership(team_id).get()                   //получаем мемберов команды по id команды
-        def mm = teamService.getTeamMember(team_id).get()                     // получаем мембера команды
+        log.debug ("Название роли которую ввел юзер:" + name_role)
 
+        def roles = teamService.getTeamRoles().get()                          // получили список всех ролей
+        log.debug("Получам список всех ролей:" + roles.collect{it.getName()}.toString())
+        def role_id
+        for (role in roles) {
+            //log.debug("Название роли которую ввел юзер:" + roles.toString())
+            if (name_role == role.getName()) {
+                log.debug ("Сравниваем роль что ввел юзер и какая есть в массиве:" + role.getName())
+                role_id = role.getId()
+                break
+            }
+        }
+        log.debug ("Получаем ID роли которую ввел юзер:" + role_id)
+        def members = teamService.getTeamMember(2) // получаем имя пользователя по его id
+        def member = members.get().getMemberName()
+        log.debug ("Получаем имя пользователя:" + member )
 
-        //List <QueryLiteral> out = mm.get()
-
+        def memberteam = teamManager.getTempoUserByMemberId(2)  // получаем темпо юзера по его id
+        def mt = memberteam.getUsername()
+        log.debug("Получаем темпо пользователей:" + mt )
+        /*
+        List<TempoUser> membrs = teamService2.getTeamMembers(team)
+        log.debug("Получаем список темпо пользователей в команде:" + membrs)
+        def mem_id
+        for (member in membrs){
+            mem_id = member.getUsername()
+            if (role_id == member.get) {
+                log.debug("Получаем список пользователей в команде:" + mem_id)
+            }
+        }
+        def qq = teamService.getGroups(team_id)
+        log.debug("Получаем какие то группы:" + qq)
+        //def ww = teamService.getTeamMember()
+        //log.debug("Получаем группы в какие входит юзер:" + ww)
+        */
+        def date = LocalDateRange.unbounded()
+        log.debug("Получаем дату:" + date)
+        log.debug("Получаем teamService:" + teamService)
+        List<TempoUser> mem = teamService.getTeamMembersByRole(date, role_id)
+        log.debug("Получаем юзеров в роли:" + mem)
 
 
 
